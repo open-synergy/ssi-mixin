83a1d38f7a5095379530b917476bf1885b7051a2
mikevhe18 <viriyananda.michael@gmail.com>
2021-10-30 20:56:34 +0700

    [ssi_multiple_approval_mixin] 11.0.1.0.0

---
+++ ssi_multiple_approval_mixin/models/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    approval_template,
+    approval_template_detail,
+    approval_approval,
+    mixin_multiple_approval,
+)
