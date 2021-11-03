2b0f039ec5fec7c6052ae765dd767153f157c482
Andhitia Rama <andhitia.r@gmail.com>
2021-11-03 15:18:06 +0700

    [ssi_policy_mixin] 11.0.1.0.0

---
+++ ssi_policy_mixin/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Policy Mixin",
+    "version": "11.0.1.0.0",
+    "category": "Administration",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "menu.xml",
+        "views/policy_template_views.xml",
+        "views/policy_template_detail_views.xml",
+    ],
+}
