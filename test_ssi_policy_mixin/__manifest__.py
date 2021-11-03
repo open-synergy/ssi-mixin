d48cdf2cc677e35d7fdffea1f9cd5c4e4250dac6
Andhitia Rama <andhitia.r@gmail.com>
2021-11-03 15:18:43 +0700

    [test_ssi_policy_mixin] 11.0.1.0.0

---
+++ test_ssi_policy_mixin/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Test Module: Workflow Policy Mixin",
+    "version": "11.0.1.0.0",
+    "category": "Administration",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "mail",
+        "ssi_policy_mixin",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/test_policy_type_view.xml",
+        "views/test_policy_view.xml",
+    ],
+}
