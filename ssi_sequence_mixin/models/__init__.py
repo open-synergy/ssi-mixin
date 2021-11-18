6e04ff1ce7e8e77b8c43fb862c02cc83d933f4ad
mikevhe18 <viriyananda.michael@gmail.com>
2021-11-18 14:42:56 +0700

    [ssi_sequence_mixin] 11.0.1.0.0

---
+++ ssi_sequence_mixin/models/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import (
+    sequence_template,
+    mixin_sequence,
+)
---
+++ ssi_sequence_mixin/models/mixin_sequence.py
@@ -0,0 +1,89 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+from odoo import _, api, models
+from odoo.exceptions import Warning as UserError
+from odoo.tools.safe_eval import safe_eval
+
+
+class MixingSequence(models.AbstractModel):
+    _name = "mixin.sequence"
+    _description = "Mixin Object for Sequence Policy"
+
+    _fallback_sequence_field = "name"
+
+    @api.multi
+    def _get_localdict(self):
+        self.ensure_one()
+        return {
+            "env": self.env,
+            "document": self,
+        }
+
+    @api.multi
+    def _evaluate_policy(self, template):
+        self.ensure_one()
+        if not template:
+            return False
+        try:
+            method_name = "_evaluate_policy_" + template.computation_method
+            result = getattr(self, method_name)(template)
+        except Exception as error:
+            msg_err = _("Error evaluating conditions.\n %s") % error
+            raise UserError(msg_err)
+        return result
+
+    @api.multi
+    def _evaluate_policy_use_python(self, template):
+        self.ensure_one()
+        res = False
+        localdict = self._get_localdict()
+        try:
+            safe_eval(template.python_code, localdict, mode="exec", nocopy=True)
+            res = localdict["result"]
+        except Exception as error:
+            raise UserError(_("Error evaluating conditions.\n %s") % error)
+        return res
+
+    @api.multi
+    def _evaluate_policy_use_domain(self, template):
+        self.ensure_one()
+        result = False
+        domain = [("id", "=", self.id)] + safe_eval(template.domain, {})
+
+        count_result = self.search_count(domain)
+        if count_result > 0:
+            result = True
+        return result
+
+    @api.multi
+    def _get_template(self):
+        result = False
+        obj_sequence_template = self.env["sequence.template"]
+        criteria = [
+            ("model_id.model", "=", str(self._name)),
+        ]
+        templates = obj_sequence_template.search(
+            criteria,
+            order="sequence desc",
+        )
+        for template in templates:
+            if self._evaluate_policy(template):
+                result = template
+                break
+        return result
+
+    @api.multi
+    def _create_sequence(self):
+        self.ensure_one()
+        template = self._get_template()
+        if template:
+            result = template.initial_string
+            if getattr(self, template.sequence_field_id.name) == result:
+                result = template.create_sequence(self)
+            else:
+                result = getattr(self, template.sequence_field_id.name)
+        else:
+            result = getattr(self, self._fallback_sequence_field)
+
+        return result
