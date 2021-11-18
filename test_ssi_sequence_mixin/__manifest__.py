b52e4485d5cca500012e405c84724c99b3f36342
mikevhe18 <viriyananda.michael@gmail.com>
2021-11-18 14:43:54 +0700

    [test_ssi_sequence_mixin] 11.0.1.0.0

---
+++ test_ssi_sequence_mixin/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Test Module - Sequence Policy Mixin",
+    "version": "11.0.1.0.0",
+    "category": "Administration",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "mail",
+        "ssi_sequence_mixin",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/test_sequence_view.xml",
+    ],
+}
