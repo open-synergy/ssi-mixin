ef11f46366bbd6c46f6a56ed1478f65d27068905
mikevhe18 <viriyananda.michael@gmail.com>
2021-12-30 17:38:05 +0700

    [ssi_state_change_history_mixin] 11.0.1.0.0

---
+++ ssi_state_change_history_mixin/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "State Change History Mixin",
+    "version": "11.0.1.0.0",
+    "category": "Administration",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "menu.xml",
+        "views/state_change_history_views.xml",
+    ],
+}
---
+++ ssi_state_change_history_mixin/models/state_change_history.py
@@ -0,0 +1,35 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class StateChangeHistory(models.Model):
+    _name = "state.change.history"
+    _description = "State Change History"
+
+    model = fields.Char(
+        string="Related Document Model",
+        index=True,
+    )
+    res_id = fields.Integer(
+        string="Related Document ID",
+        index=True,
+    )
+    user_id = fields.Many2one(
+        string="User",
+        comodel_name="res.users",
+        required=True,
+    )
+    state_from = fields.Char(
+        string="State From",
+        required=True,
+    )
+    state_to = fields.Char(
+        string="State To",
+        required=True,
+    )
+    date_change = fields.Datetime(
+        string="Date Change",
+        required=True,
+    )
