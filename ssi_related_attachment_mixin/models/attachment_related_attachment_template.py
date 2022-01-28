1f1d0adef78b82c6ac7bd82cf5367316282e8768
mikevhe18 <viriyananda.michael@gmail.com>
2022-01-28 13:40:13 +0700

    [ssi_related_attachment_mixin] 11.0.1.0.0

---
+++ ssi_related_attachment_mixin/models/attachment_related_attachment_template.py
@@ -0,0 +1,67 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class AttachmentRelatedAttachmentTemplate(models.Model):
+    _name = "attachment.related_attachment_template"
+    _description = "Related Attachment Template"
+    _order = "sequence asc, id"
+
+    @api.model
+    def _default_company_id(self):
+        return self.env["res.company"]._company_default_get(self._name)
+
+    name = fields.Char(
+        string="Name",
+        required=True,
+    )
+    model_id = fields.Many2one(
+        string="Referenced Model",
+        comodel_name="ir.model",
+        index=True,
+        required=True,
+    )
+    model = fields.Char(
+        related="model_id.model",
+        index=True,
+        store=True,
+    )
+    company_id = fields.Many2one(
+        string="Company",
+        comodel_name="res.company",
+        default=lambda self: self._default_company_id(),
+    )
+    sequence = fields.Integer(
+        default=1,
+    )
+    active = fields.Boolean(
+        default=True,
+    )
+    note = fields.Text(
+        string="Note",
+    )
+    detail_ids = fields.One2many(
+        string="Details",
+        comodel_name="attachment.related_attachment_template_detail",
+        inverse_name="template_id",
+        copy=True,
+    )
+    computation_method = fields.Selection(
+        string="Computation Method",
+        selection=[
+            ("use_domain", "Domain"),
+            ("use_python", "Python Code"),
+        ],
+        default="use_python",
+        required=True,
+    )
+    domain = fields.Char(
+        string="Domain",
+    )
+    python_code = fields.Text(
+        string="Python Code",
+        default="""# Available locals:\n#  - rec: current record""",
+    )
