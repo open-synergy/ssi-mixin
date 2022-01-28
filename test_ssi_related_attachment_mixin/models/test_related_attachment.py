dcc65bdcdb809e24d194ae20c8eed9570ca998db
mikevhe18 <viriyananda.michael@gmail.com>
2022-01-28 13:40:50 +0700

    [test_ssi_related_attachment_mixin] 11.0.1.0.0

---
+++ test_ssi_related_attachment_mixin/models/test_related_attachment.py
@@ -0,0 +1,79 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class TestRelatedAttachment(models.Model):
+    _name = "test.related_attachment"
+    _description = "Test Related Attachment"
+    _inherit = [
+        "mixin.related_attachment",
+        "mail.thread",
+    ]
+
+    name = fields.Char(
+        string="# Document",
+        default="/",
+        required=True,
+    )
+    date = fields.Date(
+        string="Date",
+        index=True,
+        readonly=True,
+        states={"draft": [("readonly", False)]},
+        copy=False,
+        default=fields.Date.context_today,
+    )
+    user_id = fields.Many2one(
+        string="Users",
+        comodel_name="res.users",
+        required=True,
+    )
+    active = fields.Boolean(
+        string="Active",
+        default=True,
+    )
+    notes = fields.Text(
+        string="Notes",
+    )
+    state = fields.Selection(
+        string="State",
+        selection=[
+            ("draft", "Draft"),
+            ("confirm", "Waiting for Approval"),
+            ("open", "On Progress"),
+            ("done", "Finished"),
+            ("cancel", "Cancelled"),
+            ("terminate", "Terminated"),
+            ("reject", "Rejected"),
+        ],
+        default="draft",
+    )
+
+    def action_confirm(self):
+        for document in self:
+            document.write({"state": "confirm"})
+
+    def action_open(self):
+        for document in self:
+            document.write({"state": "open"})
+
+    def action_done(self):
+        for document in self:
+            document.write({"state": "done"})
+
+    def action_cancel(self):
+        for document in self:
+            document.write({"state": "cancel"})
+
+    def action_restart(self):
+        for document in self:
+            document.write({"state": "draft"})
+
+    @api.onchange(
+        "user_id",
+    )
+    def onchange_related_attachment_template_id(self):
+        super(TestRelatedAttachment, self)._onchange_related_attachment_template_id()
