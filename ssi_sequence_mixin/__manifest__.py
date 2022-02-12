375506724eec00cfda2044208e872e22cfaccfe7
Andhitia Rama <andhitia.r@gmail.com>
2022-02-12 09:41:07 +0700

    [ssi_sequence_mixin] 11.0.1.2.0
    * Change AGPL -> LGPL
    * Fix menu and window action string
    * Fix menu sequence

--- ssi_sequence_mixin/__manifest__.py
+++ ssi_sequence_mixin/__manifest__.py
@@ -1,10 +1,10 @@
 # Copyright 2021 OpenSynergy Indonesia
 # Copyright 2021 PT. Simetri Sinergi Indonesia
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-# pylint: disable=locally-disabled, manifest-required-author
+
 {
     "name": "Sequence Mixin",
-    "version": "11.0.1.1.0",
+    "version": "11.0.1.2.0",
     "category": "Administration",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
