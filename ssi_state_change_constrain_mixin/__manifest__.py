525cdb4d3702128137d566490cbe027fdc1929bc
Andhitia Rama <andhitia.r@gmail.com>
2022-02-12 15:23:57 +0700

    [ssi_state_change_constrain_mixin] 11.0.1.2.0
    * Standardize template name_get
    * Template domain on mixin

--- ssi_state_change_constrain_mixin/__manifest__.py
+++ ssi_state_change_constrain_mixin/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "State Change Constrain Mixin",
-    "version": "11.0.1.1.0",
+    "version": "11.0.1.2.0",
     "category": "Administration",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
