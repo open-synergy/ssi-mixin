6a6d6cd2e25634b9485b0b6794b7c62da1513a23
Andhitia Rama <andhitia.r@gmail.com>
2022-02-12 09:56:44 +0700

    [ssi_state_change_constrain_mixin] 11.0.1.1.0
    * Change AGPL -> LGPL
    * Fix menu and window action string
    * Fix menu sequence

--- ssi_state_change_constrain_mixin/__manifest__.py
+++ ssi_state_change_constrain_mixin/__manifest__.py
@@ -1,10 +1,10 @@
 # Copyright 2021 OpenSynergy Indonesia
 # Copyright 2021 PT. Simetri Sinergi Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-# pylint: disable=locally-disabled, manifest-required-author
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 {
     "name": "State Change Constrain Mixin",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.1.0",
     "category": "Administration",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
