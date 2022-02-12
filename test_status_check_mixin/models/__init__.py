0145641f04f89da4b80ec1bb4625a8df0684b32d
Andhitia Rama <andhitia.r@gmail.com>
2022-02-12 09:51:51 +0700

    [test_status_check_mixin] 11.0.1.1.0
    * Change AGPL -> LGPL
    * Fix menu and window action string
    * Fix menu sequence

--- test_status_check_mixin/models/__init__.py
+++ test_status_check_mixin/models/__init__.py
@@ -1,6 +1,7 @@
 # Copyright 2021 OpenSynergy Indonesia
 # Copyright 2021 PT. Simetri Sinergi Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from . import (
     test_status_check,
 )
