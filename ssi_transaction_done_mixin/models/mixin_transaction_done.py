b6d5f6d04ac8352cc06ad155d321fcaa2752455f
Andhitia Rama <andhitia.r@gmail.com>
2022-03-12 19:49:02 +0700

    [ssi_transaction_done_mixin] 14.0.1.1.0

---
+++ ssi_transaction_done_mixin/models/mixin_transaction_done.py
@@ -0,0 +1,41 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class MixinTransactionDone(models.AbstractModel):
+    _name = "mixin.transaction_done"
+    _inherit = [
+        "mixin.transaction",
+    ]
+    _description = "Transaction Mixin - Done State Mixin"
+    _done_state = "done"
+
+    def _compute_policy(self):
+        _super = super(MixinTransactionDone, self)
+        _super._compute_policy()
+
+    done_ok = fields.Boolean(
+        string="Can Finished",
+        compute="_compute_policy",
+    )
+
+    def _prepare_done_data(self):
+        self.ensure_one()
+        result = {
+            "state": "done",
+        }
+        if self._create_sequence_state == self._done_state:
+            sequence = self._create_sequence()
+            result.update(
+                {
+                    "name": sequence,
+                }
+            )
+        return result
+
+    def action_done(self):
+        for record in self:
+            record.write(record._prepare_done_data())
