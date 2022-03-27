503ebe28e363dc8ebf8dedbfe26d0c234dcfed4e
Andhitia Rama <andhitia.r@gmail.com>
2022-03-27 22:03:56 +0700

    [ssi_master_data_mixin] 14.0.2.0.0
    * Inherit master data mixin from mail.activity and mail.thread
    * Check duplicate code
    * name_get can use code or not
    * copy automaticaly put (copy) on code

--- ssi_master_data_mixin/__manifest__.py
+++ ssi_master_data_mixin/__manifest__.py
@@ -3,13 +3,13 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Master Data Mixin",
-    "version": "14.0.1.1.0",
+    "version": "14.0.2.0.0",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "LGPL-3",
     "installable": True,
     "depends": [
-        "base",
+        "mail",
     ],
     "data": [
         "views/mixin_master_data_views.xml",
