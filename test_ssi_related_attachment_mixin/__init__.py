bf1241465f0c366b8659425155e0b86c2e7dc3d5
Nurazmi <azmimr67@gmail.com>
2022-04-04 23:31:19 +0700

    [test_ssi_related_attachment_mixin] 8.0.1.0.0
    * Pembuatan Baru
    * Task T006095

---
+++ test_ssi_related_attachment_mixin/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# pylint: disable=locally-disabled, manifest-required-author
+
+from . import (
+    models,
+)
