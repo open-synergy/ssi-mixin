2b857aaf6bb70ab3397f457a7dbae5b110020371
Andhitia Rama <andhitia.r@gmail.com>
2022-04-09 01:29:39 +0700

    [ssi_duration_mixin] 14.0.2.0.0
    * Use DateCallable field
    * Parameterize

--- ssi_duration_mixin/__manifest__.py
+++ ssi_duration_mixin/__manifest__.py
@@ -3,13 +3,15 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Duration Mixin",
-    "version": "14.0.1.0.0",
+    "version": "14.0.2.0.0",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "LGPL-3",
     "installable": True,
     "depends": [
-        "base",
+        "ssi_field_date_callable_attribute",
     ],
-    "data": [],
+    "data": [
+        "menu.xml",
+    ],
 }
