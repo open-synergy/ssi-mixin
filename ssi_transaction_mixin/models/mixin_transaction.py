4e8cc826487be205189ca44e0691819155d80ca8
Andhitia Rama <andhitia.r@gmail.com>
2022-05-03 09:45:22 +0700

    [ssi_transaction_mixin] 14.0.2.2.0
    * Automatic view implementation

--- ssi_transaction_mixin/models/mixin_transaction.py
+++ ssi_transaction_mixin/models/mixin_transaction.py
@@ -2,6 +2,8 @@
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+from lxml import etree
+
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
@@ -17,6 +19,12 @@ class MixinTransaction(models.AbstractModel):
     _description = "Transaction Mixin"
     _draft_state = "draft"
     _create_sequence_state = False
+    _automatically_insert_view_element = False
+    _statusbar_visible_label = "draft"
+    _policy_field_xpath = (
+        "/form/sheet/notebook/page[@name='policy']"
+        "/group[@name='policy_2']/field[@name='restart_ok']"
+    )
 
     name = fields.Char(
         string="# Document",
@@ -41,6 +49,17 @@ class MixinTransaction(models.AbstractModel):
     note = fields.Text(
         string="Note",
         copy=True,
+    )
+    state = fields.Selection(
+        string="State",
+        selection=[
+            ("draft", "Draft"),
+        ],
+        default="draft",
+        required=True,
+        copy=False,
+        readonly=True,
+        states={"draft": [("readonly", False)]},
     )
 
     def _compute_policy(self):
@@ -87,3 +106,70 @@ class MixinTransaction(models.AbstractModel):
                 raise UserError(strWarning2)
         _super = super(MixinTransaction, self)
         _super.unlink()
+
+    @api.model
+    def fields_view_get(
+        self, view_id=None, view_type="form", toolbar=False, submenu=False
+    ):
+        result = super().fields_view_get(
+            view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu
+        )
+        View = self.env["ir.ui.view"]
+
+        view_arch = etree.XML(result["arch"])
+
+        view_arch = self._reorder_header_button(view_arch, view_type)
+        view_arch = self._reorder_policy_field(view_arch, view_type)
+        view_arch = self._reconfigure_statusbar_visible(view_arch, view_type)
+
+        if view_id and result.get("base_model", self._name) != self._name:
+            View = View.with_context(base_model_name=result["base_model"])
+        new_arch, new_fields = View.postprocess_and_fields(view_arch, self._name)
+        result["arch"] = new_arch
+        new_fields.update(result["fields"])
+        result["fields"] = new_fields
+
+        return result
+
+    @api.model
+    def _add_view_element(
+        self, view_arch, qweb_template_xml_id, xpath, position="after", order=False
+    ):
+        additional_element = self.env["ir.qweb"]._render(qweb_template_xml_id)
+        node_xpath = view_arch.xpath(xpath)[0]
+        new_node = etree.fromstring(additional_element)
+        if order:
+            new_node.set("order", str(order))
+        if position == "after":
+            node_xpath.addnext(new_node)
+        elif position == "before":
+            node_xpath.addprevious(new_node)
+        return view_arch
+
+    @api.model
+    def _reorder_header_button(self, view_arch, view_type):
+        if view_type == "form" and self._automatically_insert_view_element:
+            node_xpath = view_arch.xpath("/form/header")[0]
+            to_sort = (e for e in node_xpath if e.tag == "button")
+            no_sort = (e for e in node_xpath if e.tag == "field")
+            node_xpath[:] = sorted(
+                to_sort, key=lambda child: child.get("order", 100)
+            ) + list(no_sort)
+        return view_arch
+
+    @api.model
+    def _reorder_policy_field(self, view_arch, view_type):
+        if view_type == "form" and self._automatically_insert_view_element:
+            node_xpath = view_arch.xpath(
+                "/form/sheet/notebook/page[@name='policy']/group[@name='policy_2']"
+            )[0]
+            to_sort = (e for e in node_xpath if e.tag == "field")
+            node_xpath[:] = sorted(to_sort, key=lambda child: child.get("order", 100))
+        return view_arch
+
+    @api.model
+    def _reconfigure_statusbar_visible(self, view_arch, view_type):
+        if view_type == "form" and self._automatically_insert_view_element:
+            node_xpath = view_arch.xpath("/form/header/field[@name='state']")[0]
+            node_xpath.set("statusbar_visible", self._statusbar_visible_label)
+        return view_arch
