c644736c02d8da556c94f2ea37f9f6d35ae0eb97
Andhitia Rama <andhitia.r@gmail.com>
2022-05-03 09:46:53 +0700

    [ssi_transaction_confirm_mixin] 14.0.2.1.0
    * Automatic view implementation

--- ssi_transaction_confirm_mixin/__manifest__.py
+++ ssi_transaction_confirm_mixin/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Transaction Mixin - Waiting for Approval State",
-    "version": "14.0.2.0.1",
+    "version": "14.0.2.1.0",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "LGPL-3",
@@ -12,5 +12,7 @@
         "ssi_transaction_mixin",
         "ssi_multiple_approval_mixin",
     ],
-    "data": [],
+    "data": [
+        "templates/mixin_transaction_confirm_templates.xml",
+    ],
 }
--- ssi_transaction_confirm_mixin/models/mixin_transaction_confirm.py
+++ ssi_transaction_confirm_mixin/models/mixin_transaction_confirm.py
@@ -2,7 +2,9 @@
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models
+from lxml import etree
+
+from odoo import api, fields, models
 
 
 class MixinTransactionConfirm(models.AbstractModel):
@@ -13,6 +15,14 @@ class MixinTransactionConfirm(models.AbstractModel):
     ]
     _description = "Transaction Mixin - Waiting for Approval Mixin"
     _confirm_state = "confirm"
+    _automatically_insert_confirm_policy_fields = True
+    _automatically_insert_confirm_button = True
+    _automatically_insert_approve_button = True
+    _automatically_insert_reject_button = True
+    _confirm_button_order = 10
+    _approve_button_order = 20
+    _reject_button_order = 30
+    _confirm_policy_field_order = 10
 
     def _compute_policy(self):
         _super = super(MixinTransactionConfirm, self)
@@ -45,3 +55,100 @@ class MixinTransactionConfirm(models.AbstractModel):
         return {
             "state": self._confirm_state,
         }
+
+    @api.model
+    def fields_view_get(
+        self, view_id=None, view_type="form", toolbar=False, submenu=False
+    ):
+        result = super().fields_view_get(
+            view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu
+        )
+        View = self.env["ir.ui.view"]
+
+        view_arch = etree.XML(result["arch"])
+        view_arch = self._view_add_policy_field(view_type, view_arch)
+        view_arch = self._view_add_confirm_button(view_type, view_arch)
+        view_arch = self._view_add_approve_button(view_type, view_arch)
+        view_arch = self._view_add_reject_button(view_type, view_arch)
+        view_arch = self._reorder_header_button(view_arch, view_type)
+        view_arch = self._reorder_policy_field(view_arch, view_type)
+
+        if view_id and result.get("base_model", self._name) != self._name:
+            View = View.with_context(base_model_name=result["base_model"])
+        new_arch, new_fields = View.postprocess_and_fields(view_arch, self._name)
+        result["arch"] = new_arch
+        new_fields.update(result["fields"])
+        result["fields"] = new_fields
+
+        return result
+
+    @api.model
+    def _view_add_policy_field(self, view_type, view_arch):
+        if (
+            view_type == "form"
+            and self._automatically_insert_view_element
+            and self._automatically_insert_confirm_policy_fields
+        ):
+            policy_element_templates = [
+                "ssi_transaction_confirm_mixin.confirm_policy_field",
+                "ssi_transaction_confirm_mixin.approve_policy_field",
+                "ssi_transaction_confirm_mixin.reject_policy_field",
+                "ssi_transaction_confirm_mixin.restart_approval_policy_field",
+            ]
+            for template in policy_element_templates:
+                view_arch = self._add_view_element(
+                    view_arch,
+                    template,
+                    self._policy_field_xpath,
+                    "before",
+                    self._confirm_policy_field_order,
+                )
+        return view_arch
+
+    @api.model
+    def _view_add_confirm_button(self, view_type, view_arch):
+        if (
+            view_type == "form"
+            and self._automatically_insert_view_element
+            and self._automatically_insert_confirm_button
+        ):
+            view_arch = self._add_view_element(
+                view_arch,
+                "ssi_transaction_confirm_mixin.button_confirm",
+                "/form/header/field[@name='state']",
+                "before",
+                self._confirm_button_order,
+            )
+        return view_arch
+
+    @api.model
+    def _view_add_approve_button(self, view_type, view_arch):
+        if (
+            view_type == "form"
+            and self._automatically_insert_view_element
+            and self._automatically_insert_approve_button
+        ):
+            view_arch = self._add_view_element(
+                view_arch,
+                "ssi_transaction_confirm_mixin.button_approve",
+                "/form/header/field[@name='state']",
+                "before",
+                self._approve_button_order,
+            )
+        return view_arch
+
+    @api.model
+    def _view_add_reject_button(self, view_type, view_arch):
+        if (
+            view_type == "form"
+            and self._automatically_insert_view_element
+            and self._automatically_insert_reject_button
+        ):
+            view_arch = self._add_view_element(
+                view_arch,
+                "ssi_transaction_confirm_mixin.button_reject",
+                "/form/header/field[@name='state']",
+                "before",
+                self._reject_button_order,
+            )
+        return view_arch
