67cfecb63d234464de6fa8df4265905576824127
Andhitia Rama <andhitia.r@gmail.com>
2022-05-03 09:45:22 +0700

    [ssi_transaction_mixin] 14.0.2.2.0
    * Automatic view implementation

--- ssi_transaction_mixin/__manifest__.py
+++ ssi_transaction_mixin/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Transaction Mixin",
-    "version": "14.0.2.1.1",
+    "version": "14.0.2.2.0",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "LGPL-3",
@@ -13,5 +13,7 @@
         "ssi_policy_mixin",
         "ssi_sequence_mixin",
     ],
-    "data": [],
+    "data": [
+        "views/mixin_transaction_views.xml",
+    ],
 }
