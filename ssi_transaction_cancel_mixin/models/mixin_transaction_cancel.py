c6ebede13c207701dc43e59cbacc364877f35c3f
Andhitia Rama <andhitia.r@gmail.com>
2022-05-06 22:35:09 +0700

    [ssi_transaction_cancel_mixin] 14.0.1.2.0
    * Option to automatically add filter state on search view

--- ssi_transaction_cancel_mixin/models/mixin_transaction_cancel.py
+++ ssi_transaction_cancel_mixin/models/mixin_transaction_cancel.py
@@ -20,6 +20,9 @@ class MixinTransactionCancel(models.AbstractModel):
     _cancel_button_order = 98
     _cancel_policy_field_order = 97
 
+    # Attributes related to add element on search view automatically
+    _automatically_insert_cancel_filter = True
+
     cancel_reason_id = fields.Many2one(
         string="Cancel Reason",
         comodel_name="base.cancel_reason",
@@ -67,11 +70,16 @@ class MixinTransactionCancel(models.AbstractModel):
         View = self.env["ir.ui.view"]
 
         view_arch = etree.XML(result["arch"])
-        view_arch = self._view_add_cancel_policy_field(view_type, view_arch)
-        view_arch = self._view_add_cancel_button(view_type, view_arch)
-        view_arch = self._view_add_cancel_reason(view_type, view_arch)
-        view_arch = self._reorder_header_button(view_arch, view_type)
-        view_arch = self._reorder_policy_field(view_arch, view_type)
+
+        if view_type == "form":
+            view_arch = self._view_add_cancel_policy_field(view_type, view_arch)
+            view_arch = self._view_add_cancel_button(view_type, view_arch)
+            view_arch = self._view_add_cancel_reason(view_type, view_arch)
+            view_arch = self._reorder_header_button(view_arch, view_type)
+            view_arch = self._reorder_policy_field(view_arch, view_type)
+        elif view_type == "search" and self._automatically_insert_view_element:
+            view_arch = self._add_cancel_filter_on_search_view(view_arch)
+            view_arch = self._reorder_state_filter_on_search_view(view_arch)
 
         if view_id and result.get("base_model", self._name) != self._name:
             View = View.with_context(base_model_name=result["base_model"])
@@ -81,6 +89,17 @@ class MixinTransactionCancel(models.AbstractModel):
         result["fields"] = new_fields
 
         return result
+
+    @api.model
+    def _add_cancel_filter_on_search_view(self, view_arch):
+        if self._automatically_insert_cancel_filter:
+            view_arch = self._add_view_element(
+                view_arch,
+                "ssi_transaction_cancel_mixin.cancel_filter",
+                self._state_filter_xpath,
+                "after",
+            )
+        return view_arch
 
     @api.model
     def _view_add_cancel_policy_field(self, view_type, view_arch):
