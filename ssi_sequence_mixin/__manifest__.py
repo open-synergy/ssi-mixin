11874cdd7a333568d84eba6bc7778a2226215c69
mikevhe18 <viriyananda.michael@gmail.com>
2022-09-09 13:43:49 +0700

    [ssi_sequence_mixin] 11.0.1.2.1

--- ssi_sequence_mixin/__manifest__.py
+++ ssi_sequence_mixin/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Sequence Mixin",
-    "version": "11.0.1.2.0",
+    "version": "11.0.1.2.1",
     "category": "Administration",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
