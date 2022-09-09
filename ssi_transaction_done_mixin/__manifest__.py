b2e382067df65417cfc544e1d8935cf42c3ab975
mikevhe18 <viriyananda.michael@gmail.com>
2022-09-09 13:45:29 +0700

    [ssi_transaction_done_mixin] 11.0.1.0.0

--- ssi_transaction_done_mixin/__manifest__.py
+++ ssi_transaction_done_mixin/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Transaction Mixin - Done State",
-    "version": "14.0.1.4.0",
+    "version": "11.0.1.0.0",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "LGPL-3",
