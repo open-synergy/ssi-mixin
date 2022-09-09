0298df57a8e78d131b78fcbe415ab032125d3f42
mikevhe18 <viriyananda.michael@gmail.com>
2022-09-09 13:45:07 +0700

    [ssi_transaction_confirm_mixin] 11.0.1.0.0

--- ssi_transaction_confirm_mixin/__manifest__.py
+++ ssi_transaction_confirm_mixin/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Transaction Mixin - Waiting for Approval State",
-    "version": "14.0.2.2.2",
+    "version": "11.0.1.0.0",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "LGPL-3",
