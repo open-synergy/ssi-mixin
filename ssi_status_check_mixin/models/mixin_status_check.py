5147a66f464b27b57593462b338727e64e7f39eb
mikevhe18 <viriyananda.michael@gmail.com>
2022-10-24 17:51:00 +0700

    [ssi_status_check_mixin] 14.0.1.3.0

--- ssi_status_check_mixin/models/mixin_status_check.py
+++ ssi_status_check_mixin/models/mixin_status_check.py
@@ -1,6 +1,7 @@
 # Copyright 2022 OpenSynergy Indonesia
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0-standalone.html).
+from lxml import etree
 
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
@@ -10,6 +11,9 @@ from odoo.tools.safe_eval import safe_eval
 class MixinStatusCheck(models.AbstractModel):
     _name = "mixin.status_check"
     _description = "Mixin Object for Status Check"
+
+    _status_check_create_page = True
+    _status_check_page_xpath = "//page[last()]"
 
     status_check_template_id = fields.Many2one(
         string="# Status Check Template",
@@ -23,6 +27,33 @@ class MixinStatusCheck(models.AbstractModel):
         domain=lambda self: [("model", "=", self._name)],
         auto_join=True,
     )
+
+    @api.model
+    def fields_view_get(
+        self, view_id=None, view_type="form", toolbar=False, submenu=False
+    ):
+        res = super().fields_view_get(
+            view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu
+        )
+        if view_type == "form" and self._status_check_create_page:
+            doc = etree.XML(res["arch"])
+            node_xpath = doc.xpath(self._status_check_page_xpath)
+            str_element = self.env["ir.qweb"]._render(
+                "ssi_status_check_mixin.status_check_page"
+            )
+            for node in node_xpath:
+                new_node = etree.fromstring(str_element)
+                node.addnext(new_node)
+
+            View = self.env["ir.ui.view"]
+
+            if view_id and res.get("base_model", self._name) != self._name:
+                View = View.with_context(base_model_name=res["base_model"])
+            new_arch, new_fields = View.postprocess_and_fields(doc, self._name)
+            res["arch"] = new_arch
+            new_fields.update(res["fields"])
+            res["fields"] = new_fields
+        return res
 
     def _prepare_status_check_data(self, template_id, template_detail_id):
         self.ensure_one()
@@ -123,3 +154,14 @@ class MixinStatusCheck(models.AbstractModel):
                 )
                 res += obj_status_check.create(data)
         return res
+
+    @api.model
+    def create(self, values):
+        _super = super(MixinStatusCheck, self)
+        result = _super.create(values)
+        if not result.status_check_template_id:
+            template_id = result._get_template_status_check()
+            if template_id:
+                result.write({"status_check_template_id": template_id})
+                result.onchange_status_check_ids()
+        return result
