8fc5db096f8e0921dafa26a33112c51e85353b12
Andhitia Rama <andhitia.r@gmail.com>
2023-01-22 00:12:02 +0700

    [ssi_master_data_mixin] 14.0.3.0.0

--- ssi_master_data_mixin/__manifest__.py
+++ ssi_master_data_mixin/__manifest__.py
@@ -3,13 +3,14 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Master Data Mixin",
-    "version": "14.0.2.1.0",
+    "version": "14.0.3.0.0",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "LGPL-3",
     "installable": True,
     "depends": [
         "mail",
+        "ssi_print_mixin",
     ],
     "data": [
         "views/mixin_master_data_views.xml",
