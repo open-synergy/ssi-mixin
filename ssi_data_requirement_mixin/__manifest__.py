3b06ef53a825f8d57bf5dbdbfee7acf4089b98b9
Andhitia Rama <andhitia.r@gmail.com>
2023-10-07 21:35:23 +0700

    [UPD] ssi_data_requirement_mixin
    * Use new decorator for field_view_get
    * Change license into AGPL

--- ssi_data_requirement_mixin/__manifest__.py
+++ ssi_data_requirement_mixin/__manifest__.py
@@ -1,13 +1,13 @@
 # Copyright 2022 OpenSynergy Indonesia
 # Copyright 2022 PT. Simetri Sinergi Indonesia
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Data Requirement Mixin",
     "version": "14.0.2.6.1",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "installable": True,
     "application": True,
     "depends": [
