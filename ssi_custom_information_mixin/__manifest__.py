bb311b9c626c796d2801518a7d32e5da9eb205a7
Andhitia Rama <andhitia.r@gmail.com>
2023-10-07 21:31:15 +0700

    [UPD] ssi_custom_information_mixin
    * Use new decorator for field_view_get
    * Change license into AGPL

--- ssi_custom_information_mixin/__manifest__.py
+++ ssi_custom_information_mixin/__manifest__.py
@@ -1,16 +1,16 @@
 # Copyright 2022 OpenSynergy Indonesia
 # Copyright 2022 PT. Simetri Sinergi Indonesia
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Custom Information Mixin",
     "version": "14.0.2.0.0",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "category": "Tools",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "installable": True,
     "depends": [
-        "base",
+        "ssi_decorator",
     ],
     "data": [
         "security/ir.model.access.csv",
