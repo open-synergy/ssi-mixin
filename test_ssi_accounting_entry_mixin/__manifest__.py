6145be5d430b495bee72c95a9daca5294d44dd5c
Andhitia Rama <andhitia.r@gmail.com>
2023-12-30 21:01:44 +0700

    [UPD] test_ssi_accounting_entry_mixin
    * Change license into AGPL

--- test_ssi_accounting_entry_mixin/__manifest__.py
+++ test_ssi_accounting_entry_mixin/__manifest__.py
@@ -1,13 +1,13 @@
 # Copyright 2022 OpenSynergy Indonesia
 # Copyright 2022 PT. Simetri Sinergi Indonesia
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Test Module: Accounting Entry Mixin",
     "version": "14.0.2.0.0",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "category": "Tools",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "installable": True,
     "depends": [
         "ssi_accounting_entry_mixin",
