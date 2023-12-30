c308137e040a0c4db832e8c7749896a0065fa8b2
Andhitia Rama <andhitia.r@gmail.com>
2023-12-30 21:22:49 +0700

    [UPD] ssi_related_attachment_mixin
    * Change license into AGPL

--- ssi_related_attachment_mixin/__manifest__.py
+++ ssi_related_attachment_mixin/__manifest__.py
@@ -1,6 +1,6 @@
 # Copyright 2022 OpenSynergy Indonesia
 # Copyright 2022 PT. Simetri Sinergi Indonesia
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     "name": "Related Attachment Mixin",
@@ -8,7 +8,7 @@
     "category": "Administration",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "installable": True,
     "depends": [
         "ssi_decorator",
