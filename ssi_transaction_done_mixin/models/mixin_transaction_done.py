0b73eb98c36f1912d630745ba651f348dc800980
Andhitia Rama <andhitia.r@gmail.com>
2024-04-10 06:49:38 +0700

    [UPD] ssi_transaction_done_mixin
    * Fix bypass policy check param name

--- ssi_transaction_done_mixin/models/mixin_transaction_done.py
+++ ssi_transaction_done_mixin/models/mixin_transaction_done.py
@@ -108,7 +108,7 @@ class MixinTransactionDone(models.AbstractModel):
 
     def _check_done_policy(self):
         self.ensure_one()
-        if self.env.context.get("bypass_done_policy", False):
+        if self.env.context.get("bypass_policy_check", False):
             return True
 
         if not self.done_ok:
