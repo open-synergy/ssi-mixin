9c74a5d0fc3397efed336b3b4fb89e2ff058853b
Andhitia Rama <andhitia.r@gmail.com>
2024-04-13 08:39:36 +0700

    [UPD] ssi_product_line_price_mixin
    * Add standard price and diff info

--- ssi_product_line_price_mixin/models/mixin_product_line_price.py
+++ ssi_product_line_price_mixin/models/mixin_product_line_price.py
@@ -12,29 +12,12 @@ class MixinProductLinePrice(models.AbstractModel):
         "mixin.product_line",
     ]
 
-    @api.model
-    def _default_currency_id(self):
-        return self.env.company.currency_id.id
-
     currency_id = fields.Many2one(
         string="Currency",
         comodel_name="res.currency",
         required=True,
         default=lambda self: self._default_currency_id(),
     )
-
-    @api.depends(
-        "currency_id",
-    )
-    def _compute_allowed_pricelist_ids(self):
-        Pricelist = self.env["product.pricelist"]
-        for record in self:
-            result = []
-            if record.currency_id:
-                criteria = record._get_pricelist_domain()
-                result = Pricelist.search(criteria).ids
-            record.allowed_pricelist_ids = result
-
     allowed_pricelist_ids = fields.Many2many(
         string="Allowed Pricelists",
         comodel_name="product.pricelist",
@@ -51,6 +34,88 @@ class MixinProductLinePrice(models.AbstractModel):
         required=True,
         default=0.0,
     )
+    price_subtotal = fields.Monetary(
+        string="Price Subtotal",
+        currency_field="currency_id",
+        compute="_compute_price",
+        store=True,
+    )
+    standard_price_unit = fields.Monetary(
+        string="Standard Price Unit",
+        currency_field="currency_id",
+        compute="_compute_standard_price",
+        store=True,
+        compute_sudo=True,
+    )
+    standard_price_subtotal = fields.Monetary(
+        string="Standard Price Subtotal",
+        currency_field="currency_id",
+        compute="_compute_standard_price",
+        store=True,
+        compute_sudo=True,
+    )
+    standard_price_unit_diff = fields.Monetary(
+        string="Standard Price Unit Diff.",
+        currency_field="currency_id",
+        compute="_compute_standard_price",
+        store=True,
+        compute_sudo=True,
+    )
+    standard_price_subtotal_diff = fields.Monetary(
+        string="Standard Price Subtotal Diff.",
+        currency_field="currency_id",
+        compute="_compute_standard_price",
+        store=True,
+        compute_sudo=True,
+    )
+
+    @api.model
+    def _default_currency_id(self):
+        return self.env.company.currency_id.id
+
+    @api.depends(
+        "product_id",
+        "pricelist_id",
+        "price_unit",
+        "uom_quantity",
+        "uom_id",
+    )
+    def _compute_standard_price(self):
+        for record in self:
+            standard_price_unit = (
+                standard_price_subtotal
+            ) = standard_price_unit_diff = standard_price_subtotal_diff = 0.0
+            if record.pricelist_id and record.product_id:
+                product_context = dict(
+                    self.env.context, uom=record.uom_id and record.uom_id.id or False
+                )
+                final_price, rule_id = record.pricelist_id.with_context(
+                    product_context
+                ).get_product_price_rule(
+                    record.product_id, record.uom_quantity or 1.0, False
+                )
+                standard_price_unit = final_price
+                standard_price_subtotal = standard_price_unit * record.uom_quantity
+                standard_price_unit_diff = record.price_unit - standard_price_unit
+                standard_price_subtotal_diff = (
+                    record.price_subtotal - standard_price_subtotal
+                )
+            record.standard_price_unit = standard_price_unit
+            record.standard_price_subtotal = standard_price_subtotal
+            record.standard_price_unit_diff = standard_price_unit_diff
+            record.standard_price_subtotal_diff = standard_price_subtotal_diff
+
+    @api.depends(
+        "currency_id",
+    )
+    def _compute_allowed_pricelist_ids(self):
+        Pricelist = self.env["product.pricelist"]
+        for record in self:
+            result = []
+            if record.currency_id:
+                criteria = record._get_pricelist_domain()
+                result = Pricelist.search(criteria).ids
+            record.allowed_pricelist_ids = result
 
     @api.depends(
         "price_unit",
@@ -59,13 +124,6 @@ class MixinProductLinePrice(models.AbstractModel):
     def _compute_price(self):
         for record in self:
             record.price_subtotal = record.price_unit * record.uom_quantity
-
-    price_subtotal = fields.Monetary(
-        string="Price Subtotal",
-        currency_field="currency_id",
-        compute="_compute_price",
-        store=True,
-    )
 
     @api.onchange(
         "allowed_pricelist_ids",
