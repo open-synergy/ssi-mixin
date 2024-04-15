bd3782a80de0404de55a10ac40afa3940c156309
Andhitia Rama <andhitia.r@gmail.com>
2024-04-15 08:50:23 +0700

    [UPD] ssi_transaction_terminate_mixin
    * Always pass check policy when button is not automatically insert

--- ssi_transaction_terminate_mixin/models/mixin_transaction_terminate.py
+++ ssi_transaction_terminate_mixin/models/mixin_transaction_terminate.py
@@ -113,6 +113,10 @@ class MixinTransactionTerminate(models.AbstractModel):
 
     def _check_terminate_policy(self):
         self.ensure_one()
+
+        if not self._automatically_insert_terminate_button:
+            return True
+
         if self.env.context.get("bypass_policy_check", False):
             return True
 
