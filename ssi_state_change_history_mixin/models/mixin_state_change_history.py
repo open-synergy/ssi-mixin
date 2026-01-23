7b2e81639b24d7de4e74b09fb81c8953e8450373
Andhitia Rama <andhitia.r@gmail.com>
2026-01-23 21:51:13 +0700

    [UPD] ssi_state_change_history_mixin
    * Pre-commit fix

--- ssi_state_change_history_mixin/models/mixin_state_change_history.py
+++ ssi_state_change_history_mixin/models/mixin_state_change_history.py
@@ -25,16 +25,14 @@ class MixinStateChangeHistory(models.AbstractModel):
     def _prepare_state_change_history(self, state_to, cancel_reason_id):
         self.ensure_one()
         obj_base_cancel_reason = self.env["base.cancel_reason"]
-        Model = self.env["ir.model"]
-        models = Model.search([("model", "=", self._name)])
+        ir_model = self.env["ir.model"]
+        model_records = ir_model.search([("model", "=", self._name)])
         reason = "-"
         if cancel_reason_id:
-            reason_id = obj_base_cancel_reason.search([
-                ("id", "=", cancel_reason_id)
-            ])
+            reason_id = obj_base_cancel_reason.search([("id", "=", cancel_reason_id)])
             reason = reason_id.name
         values = {
-            "model_id": models[0].id,
+            "model_id": model_records[0].id,
             "res_id": self.id,
             "user_id": self.env.user.id,
             "state_from": self.state,
@@ -47,7 +45,9 @@ class MixinStateChangeHistory(models.AbstractModel):
     def create_state_change_history(self, state_to, cancel_reason_id=False):
         self.ensure_one()
         obj_state_change_history = self.env["state_change_history"]
-        obj_state_change_history.create(self._prepare_state_change_history(state_to, cancel_reason_id))
+        obj_state_change_history.create(
+            self._prepare_state_change_history(state_to, cancel_reason_id)
+        )
         return True
 
     def write(self, vals):
@@ -57,10 +57,10 @@ class MixinStateChangeHistory(models.AbstractModel):
                     rec.sudo().create_state_change_history(
                         vals.get("state", False),
                         vals.get("cancel_reason_id", False),
-                    )   
+                    )
                 else:
                     rec.sudo().create_state_change_history(vals.get("state", False))
-        return super(MixinStateChangeHistory, self).write(vals)
+        return super().write(vals)
 
     @ssi_decorator.insert_on_form_view()
     def _state_change_history_insert_form_element(self, view_arch):
