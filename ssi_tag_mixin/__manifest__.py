dd84914a46fd5b237b2f71ec27947cd486443850
Andhitia Rama <andhitia.r@gmail.com>
2026-07-17 16:23:20 +0700

    [UPD] ssi_tag_mixin
    * Perbaiki URL lisensi header copyright dari lgpl ke agpl sesuai standar SSI.

--- ssi_tag_mixin/__manifest__.py
+++ ssi_tag_mixin/__manifest__.py
@@ -1,6 +1,6 @@
 # Copyright 2024 OpenSynergy Indonesia
 # Copyright 2024 PT. Simetri Sinergi Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Tag Mixin",
     "version": "14.0.1.0.0",
