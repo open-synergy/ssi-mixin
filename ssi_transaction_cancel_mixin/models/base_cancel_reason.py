cccde9ee6dabd95d235dca394033309480eb869c
Andhitia Rama <andhitia.r@gmail.com>
2026-07-18 12:16:39 +0700

    [ADD] ssi_transaction_cancel_mixin
    Port modul Transaction Cancel Mixin ke Odoo 19. Penyesuaian: fields_view_get menjadi _get_view, xpath /tree menjadi /list, attrs inline, group pada ACL.

---
+++ ssi_transaction_cancel_mixin/models/base_cancel_reason.py
@@ -0,0 +1,36 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class BaseCancelReason(models.Model):
+    """
+    Master-data model that stores the available reasons for cancelling a
+    transaction. Linked to models via ``ir.model.cancel_reason_ids`` and used
+    as a required field by ``mixin.transaction_cancel``.
+    """
+
+    _name = "base.cancel_reason"
+    _description = "Cancel Reason"
+
+    name = fields.Char(
+        string="Cancel Reason",
+        required=True,
+    )
+    code = fields.Char(
+        string="Code",
+        required=True,
+    )
+    active = fields.Boolean(
+        string="Active",
+        default=True,
+    )
+    note = fields.Text(
+        string="Note",
+    )
+    global_use = fields.Boolean(
+        string="Global Use",
+        default=False,
+    )
