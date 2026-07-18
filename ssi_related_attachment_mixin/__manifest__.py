8f32606f3c8ba489b0120d4aa17bd70050a9c14f
Andhitia Rama <andhitia.r@gmail.com>
2026-07-18 10:28:35 +0700

    [ADD] ssi_related_attachment_mixin
    Port modul Related Attachment Mixin ke Odoo 19. Penyesuaian: create model_create_multi, name_get menjadi _compute_display_name, safe_eval tanpa nocopy, attrs inline, chatter, group pada ACL.

---
+++ ssi_related_attachment_mixin/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Related Attachment Mixin",
+    "version": "19.0.1.0.0",
+    "category": "Administration",
+    "website": "https://github.com/open-synergy/ssi-mixin",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_decorator",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "menu.xml",
+        "templates/related_attachment_templates.xml",
+        "wizards/attachment_related_attachment_import.xml",
+        "views/ir_model_views.xml",
+        "views/attachment_related_attachment_category_views.xml",
+        "views/attachment_related_attachment_template_detail_views.xml",
+        "views/attachment_related_attachment_template_views.xml",
+        "views/attachment_related_attachment.xml",
+    ],
+}
