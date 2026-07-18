115d22b0ca089cef7cd19d9027d92b9287c404c9
Andhitia Rama <andhitia.r@gmail.com>
2026-07-18 10:55:53 +0700

    [ADD] ssi_state_change_history_mixin
    Port modul State Change History Mixin ke Odoo 19. Penyesuaian: view list, group pada ACL, hapus auto_join.

---
+++ ssi_state_change_history_mixin/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "State Change History Mixin",
+    "version": "19.0.1.0.0",
+    "category": "Administration",
+    "website": "https://github.com/open-synergy/ssi-mixin",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "base",
+        "ssi_decorator",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "menu.xml",
+        "templates/mixin_state_change_history_templates.xml",
+        "views/state_change_history_views.xml",
+    ],
+}
