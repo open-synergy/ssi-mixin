ca8951e21167f25a84edd29f99852aa7e36be64d
Andhitia Rama <andhitia.r@gmail.com>
2026-07-18 19:33:42 +0700

    [UPD] ssi_related_attachment_mixin
    * attachment.related_attachment_category kini mewarisi
      mixin.master_data, tidak lagi mendeklarasikan ulang
      name/code/active; field description dipertahankan sebagai page
      tersendiri
    * View tree/search/form dijadikan primary view yang inherit dari
      ssi_master_data_mixin.mixin_master_data_view_{tree,search,form}
    * Menambahkan unit test skenario odoo-yaml-test (create, duplicate
      code, copy)
    
    Ref #329

---
+++ ssi_related_attachment_mixin/tests/test_attachment_related_attachment_category.py
@@ -0,0 +1,15 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo_yaml_test import YamlTransactionCase
+
+from odoo.tests import tagged
+
+
+@tagged("post_install", "-at_install")
+class TestAttachmentRelatedAttachmentCategory(YamlTransactionCase):
+    def test_attachment_related_attachment_category(self):
+        self.run_yaml_scenario(
+            "test_data_attachment_related_attachment_category.yaml"
+        )
